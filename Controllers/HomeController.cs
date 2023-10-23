8e0a7b1083ed942ef8e91b62053750783528644b
Guruprasad S K <115786145+Guruprasad-SK@users.noreply.github.com>
2023-10-23 22:14:46 +0530

    Add files via upload

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using StrongInlinehHTML.Models;
+
+namespace StrongInlinehHTML.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            Employee employee = new Employee()
+            {
+               Address = "Shivamogga",
+               Name = "Vinay",
+               isEmpty = true
+
+        };
+           
+            return View(employee);
+        }
+        [HttpPost]
+        public ActionResult Index(Employee employee)
+        {
+            return View();
+        }
+
+    }
+}
---
+++ Models/Employee.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StrongInlinehHTML.Models
+{
+    public class Employee
+    {
+        public string Name {  get; set; }
+        public string Address { get; set; }
+        public bool isEmpty { get; set; }
+    }
+}
